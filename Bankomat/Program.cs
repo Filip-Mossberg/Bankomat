9c374b0818f1fdb12d8be821632369b03dafd6ab
Filip Mossberg <filip.mossberg@gmail.com>
2022-10-04 21:03:10 +0200

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+        public static void Welcome()
+        {
+            String[] Welcome = { "Welcome to the bank!", "Welcome to your commercial bank!", "Welcome to your local bank!" };
+            Random message = new Random();
+            int number = message.Next(0, 4);
+            Console.WriteLine(Welcome[number]);
+        }
+    }
+}
