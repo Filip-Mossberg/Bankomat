d717e7f4f370ff1e4058cf04e4861a312a820bea
Filip Mossberg <filip.mossberg@gmail.com>
2022-10-04 21:39:30 +0200

    Created an 2d String Array containing 5 user accounts.

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -7,6 +7,8 @@ namespace Bankomat
         static void Main(string[] args)
         {
             Welcome();
+            string[,] accounts = Accounts();
+            Login();
         }
         public static void Welcome()
         {
@@ -14,6 +16,20 @@ namespace Bankomat
             Random message = new Random();
             int number = message.Next(0, 4);
             Console.WriteLine(Welcome[number]);
+        }
+        public static string[,] Accounts()
+        {
+            String[,] accounts = new string[5,2];
+            accounts[0, 0] = "Filip"; accounts[0, 1] = "2124";
+            accounts[1, 0] = "Noah"; accounts[1, 1] = "4452";
+            accounts[2, 0] = "Bella"; accounts[2, 1] = "9832";
+            accounts[3, 0] = "Joakim"; accounts[3, 1] = "2532";
+            accounts[4, 0] = "Ulrika"; accounts[4, 1] = "2435";
+            return accounts;
+        }
+        public static void Login()
+        {
+
         }
     }
 }
