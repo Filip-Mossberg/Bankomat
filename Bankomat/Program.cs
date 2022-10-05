de8916fb3fc34487b7ca928ded38e72ce1c19ffd
Filip Mossberg <filip.mossberg@gmail.com>
2022-10-05 18:55:53 +0200

    Added a login feuture

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http;
+using System.Reflection.Metadata;
 
 namespace Bankomat
 {
@@ -11,7 +14,8 @@ namespace Bankomat
             startprogram(program);
             Welcome();
             string[,] accounts = Accounts();
-            Login(accounts);
+            int user = Login(accounts);
+            Console.WriteLine(user);
         }
         public static bool startprogram(bool program)
         {
@@ -28,16 +32,52 @@ namespace Bankomat
         public static string[,] Accounts()
         {
             String[,] accounts = new string[5,2];
-            accounts[0, 0] = "Filip"; accounts[0, 1] = "2124";
-            accounts[1, 0] = "Noah"; accounts[1, 1] = "4452";
-            accounts[2, 0] = "Bella"; accounts[2, 1] = "9832";
-            accounts[3, 0] = "Joakim"; accounts[3, 1] = "2532";
-            accounts[4, 0] = "Ulrika"; accounts[4, 1] = "2435";
+            accounts[0, 0] = "filip"; accounts[0, 1] = "2124";
+            accounts[1, 0] = "noah"; accounts[1, 1] = "4452";
+            accounts[2, 0] = "bella"; accounts[2, 1] = "9832";
+            accounts[3, 0] = "joakim"; accounts[3, 1] = "2532";
+            accounts[4, 0] = "ulrika"; accounts[4, 1] = "2435";
             return accounts;
         }
-        public static void Login(string[,] accounts)
+        public static int Login(string[,] accounts)
         {
-
+            try
+            {
+                bool check = false;
+                int count = 0;
+                for (int attempt = 0; attempt < 3; attempt++)
+                {
+                    Console.Write("Enter your username:");
+                    var name = Console.ReadLine();
+                    Console.Write("Enter your pin-code:");
+                    var pincode = Console.ReadLine();
+                    for (count = 0; count <= 4; count++)
+                    {
+                        check = (name == accounts[count, 0] && pincode == accounts[count, 1]) ? true : false;
+                        if (check == true)
+                        {
+                            int user = count;
+                            return count;
+                        }
+                        else if (count == 4 && check == false)
+                        {
+                            Console.WriteLine("Wrong username or pin-code!");
+                            
+                        }
+                    }
+                    if (attempt == 2 && check == false)
+                    {
+                        Console.WriteLine("To many attempts!");
+                        return -1;
+                    }
+                }
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
         }
     }
 }
